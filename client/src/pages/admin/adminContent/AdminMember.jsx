54806f28285d60a910b7df2657302aab9e309860
jyp <wkdrn6736@gmail.com>
2025-05-29 01:05:24 +0900

    관리자페이지 UI 및 css 추가

--- client/src/pages/admin/adminContent/AdminMember.jsx
+++ client/src/pages/admin/adminContent/AdminMember.jsx
@@ -1,5 +1,79 @@
+import React, { useEffect, useState } from "react";
+
+import adminMemberStyle from "./AdminMember.module.css";
+
 function AdminMember() {
-  return <div>광덕정 사원 정보</div>;
+  const [searchType, setSearchType] = useState("all");
+  const [searchKeyword, setSearchKeyword] = useState("");
+
+  return (
+    <div className="record-page">
+      <div className={adminMemberStyle["record-container"]}>
+        <div className={adminMemberStyle["record-card"]}>
+          {/* 검색 영역 */}
+          <div className={adminMemberStyle["search-area"]}>
+            <div className={adminMemberStyle["search-group"]}>
+              <div className={adminMemberStyle["search-field"]}>
+                <select
+                  value={searchType}
+                  onChange={(e) => setSearchType(e.target.value)}
+                >
+                  <option value="all">전체</option>
+                  <option value="name">이름</option>
+                  <option value="memberNo">사원번호</option>
+                  <option value="role">직책</option>
+                </select>
+              </div>
+              <div className={adminMemberStyle["search-field"]}>
+                <input
+                  type="text"
+                  className="custom-input"
+                  value={searchKeyword}
+                  onChange={(e) => setSearchKeyword(e.target.value)}
+                ></input>
+              </div>
+              <div className={adminMemberStyle["search-btn-area"]}>
+                <button>검색</button>
+              </div>
+            </div>
+          </div>
+          {/* 버튼 영역 */}
+          <div className={adminMemberStyle["btn-area"]}>
+            <button>등록</button>
+            <button>삭제</button>
+            <button>수정</button>
+          </div>
+
+          {/* 회원 목록 테이블 */}
+          <div className={adminMemberStyle["member-table-container"]}>
+            <table className={adminMemberStyle["member-table"]}>
+              <colgroup>
+                <col style={{ width: "5%" }} />
+                <col style={{ width: "15%" }} />
+                <col style={{ width: "17%" }} />
+                <col style={{ width: "14%" }} />
+                <col style={{ width: "17%" }} />
+                <col style={{ width: "16%" }} />
+                <col style={{ width: "16%" }} />
+              </colgroup>
+              <thead>
+                <tr>
+                  <th>체크박스</th>
+                  <th>이름</th>
+                  <th>사원번호</th>
+                  <th>직책</th>
+                  <th>우궁/좌궁</th>
+                  <th>평균시수</th>
+                  <th>월별보기</th>
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AdminMember;
--- client/src/pages/admin/adminContent/AdminRecord.jsx
+++ client/src/pages/admin/adminContent/AdminRecord.jsx
@@ -1,5 +1,102 @@
+import React, { useRef, useState } from "react";
+
+import adminRecordStyle from "./AdminRecord.module.css";
+import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from "react-datepicker"; // npm install react-datepicker
+import { format } from "date-fns"; // npm install date-fns
+import { ko } from "date-fns/locale";
+
 function AdminRecord() {
-  return <div>광덕정 편사 기록</div>;
+  const [date, setDate] = useState(new Date());
+  const [searchName, setSearchName] = useState("");
+  const [score, setScore] = useState("");
+  const inputRef = useRef(null);
+
+  /* 시수에는 1~5 숫자만 입력되도록함 */
+  const onlyNumber = (e) => {
+    var value = e.target.value;
+    value = value.replace(/[^0-5.]/g, "").replace(/(\..*)\./g, "$1");
+    setScore(value);
+  };
+
+  return (
+    <div className="record-page">
+      <div className={adminRecordStyle["record-container"]}>
+        <div className={adminRecordStyle["record-card"]}>
+          <div className={adminRecordStyle["record-date"]}>
+            <span>📝 날짜 : </span>
+            <div className="record-datepicker-wapper" style={{ width: "40%" }}>
+              <DatePicker
+                selected={date}
+                onChange={(date) => setDate(date)}
+                dateFormat="yyyy-MM-dd"
+                locale={ko}
+                className="custom-input"
+                calendarClassName="custom-calendar"
+              />
+            </div>
+          </div>
+          <div className={adminRecordStyle["record-name"]}>
+            <span>📝 사원이름 : </span>
+            <div className="record-input-wapper" style={{ width: "40%" }}>
+              <input
+                type="text"
+                list="memberNameList"
+                className="custom-input"
+                value={searchName}
+              />
+            </div>
+          </div>
+
+          <div className={adminRecordStyle["record-score"]}>
+            <span>📝 시수 : </span>
+            <div className="record-input-wapper" style={{ width: "40%" }}>
+              <input
+                value={score}
+                type="text"
+                onChange={onlyNumber}
+                maxLength={1}
+                className="custom-input"
+                ref={inputRef}
+                style={{ width: "6vw" }}
+              />
+              <button type="submit"> 입력 </button>
+            </div>
+          </div>
+
+          <div className={adminRecordStyle["record-avgScore"]}>
+            <span>📝 평균 시수 : </span>
+            <div className="record-input-wapper" style={{ width: "40%" }}>
+              <p>평균값</p>
+            </div>
+          </div>
+
+          <div className={adminRecordStyle["record-totalScore"]}>
+            <div className={adminRecordStyle["record-totalScore-header"]}>
+              <table className={adminRecordStyle["record-table"]}>
+                <colgroup>
+                  <col style={{ width: "20%" }} />
+                  <col style={{ width: "50%" }} />
+                  <col style={{ width: "30%" }} />
+                </colgroup>
+                <thead>
+                  <tr>
+                    <th>순</th>
+                    <th>시수</th>
+                    <th>삭제</th>
+                  </tr>
+                </thead>
+                <tbody></tbody>
+              </table>
+            </div>
+          </div>
+          <div className={adminRecordStyle["score-btn-area"]}>
+            <button className="btn">저장</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AdminRecord;
